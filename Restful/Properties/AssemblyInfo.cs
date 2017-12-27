4081c107b36e6b3996416245a3256a8bdcc3820f
Enerdy <rodrigo.rente@live.com>
2017-12-27 12:55:03 +0000

    Generic plugin class & set AssemblyInfo

--- Restful/Properties/AssemblyInfo.cs
+++ Restful/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -8,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Restful")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Pryaxis")]
 [assembly: AssemblyProduct("Restful")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Pryaxis 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -27,10 +26,9 @@ using System.Runtime.InteropServices;
 //      Major Version
 //      Minor Version
 //      Build Number
-//      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
--- Restful/Restful.cs
+++ Restful/Restful.cs
@@ -1,13 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
+using TerrariaApi.Server;
 
 namespace Restful
 {
-	public class Restful
+	/// <summary>
+	/// The plugin class for managing the RESTful API.
+	/// </summary>
+	[ApiVersion(2, 1)]
+	public class Restful : TerrariaPlugin
 	{
+		/// <inheritdoc/>
+		public override string Author => "Pryaxis";
 
+		/// <inheritdoc/>
+		public override string Description => "Provides a RESTful API to manage your TShock Server remotely.";
+
+		/// <inheritdoc/>
+		public override string Name => "Restful";
+
+		/// <inheritdoc/>
+		public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+		/// <summary>
+		/// Creates a new instance of this plugin bound to a <see cref="Main"/> instance.
+		/// </summary>
+		/// <param name="game">The <see cref="Main"/> instance.</param>
+		public Restful(Main game) : base(game)
+		{
+
+		}
+
+		/// <inheritdoc/>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+
+			}
+		}
+
+		/// <inheritdoc/>
+		public override void Initialize()
+		{
+			
+		}
 	}
 }
